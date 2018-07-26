72dddd04ccc5eb3cef53851271429a72d3f5d422
Hoang Lam <hoanglam1906@gmail.com>
2018-07-27 02:00:11 +0700

    update_profile + authorize

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [:show]
+  before_action :find_user, only: [:edit, :show, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
     @user = User.new
@@ -21,18 +23,37 @@ class UsersController < ApplicationController
     redirect_to root_url && return unless @user.activated == true
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".update_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "not_found"
+    redirect_to root_path
+  end
+
   private
   def user_params
     params.require(:user).permit :name, :gender, :birthday, :address,
       :phone_number, :email, :password, :password_confirmation
   end
 
-  def find_user
-    @user = User.find_by id: params[:id]
+  def logged_in_user
+    return if logged_in?
+    flash[:danger] = t ".log_msg"
+    redirect_to login_url
+  end
 
-    return if @user
-
-    flash[:danger] = t "not_found"
-    redirect_to root_path
+  def correct_user
+    redirect_to root_url unless @user.current_user? current_user
   end
 end
