4f41dfcbd0f0675ac1ede5584233a1e83f1d9527
duongngv <duong147nguyen@gmail.com>
2018-07-27 00:35:16 +0700

    Sign up: simple

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t "welcome"
+      redirect_to root_path
+    else
+      flash[:danger] = t ".create_failed"
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+
+    return if @user
+    flash[:danger] = t ".not_found"
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit :name, :gender, :birthday, :address,
+      :phone_number, :email, :password, :password_confirmation
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,42 @@
 class User < ApplicationRecord
   enum user_type: {employee: 0, customer: 1}
+  enum gender: {male: 1, female: 2, trans: 3}
   has_one :cart
   has_many :orders, dependent: :destroy
   has_many :employees
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  before_save :email_downcase
+  has_secure_password
+
+  validates :name, presence: true,
+    length: {maximum: Settings.name_max_length}
+  validates :gender,  presence: true, inclusion: {in: genders.keys}
+  validates :birthday, presence: true,
+    inclusion: {in: (Date.today - 100.years..Date.today)}
+  validates :address, presence: true,
+    length: {maximum: Settings.address_max_length}
+  validates :phone_number, presence: true,
+    length: {in:
+      Settings.phone_number_min_length..Settings.phone_number_max_length}
+  validates :email, presence: true,
+    length: {maximum: Settings.email_max_length},
+    format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
+  validates :password, presence: true, allow_nil: true,
+    length: {minimum: Settings.password_min_length}
+
+  class << self
+    def gender_attributes_for_select
+      genders.map do |gender, _|
+        [I18n.t("activerecord.attributes.#{model_name.i18n_key}.genders.#{gender}"), gender]
+      end
+    end
+  end
+
+  private
+
+  def email_downcase
+    self.email = email.downcase!
+  end
 end
