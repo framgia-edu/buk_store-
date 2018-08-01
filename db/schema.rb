18765088cd7abb50bd2c13e3f39ac15b3d84fa5f
duongngv <duong147nguyen@gmail.com>
2018-08-01 08:24:27 +0700

    Remember me

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_26_182450) do
+ActiveRecord::Schema.define(version: 2018_08_01_011928) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "category_id"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2018_07_26_182450) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
